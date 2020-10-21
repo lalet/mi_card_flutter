9993e815aaa38b3da7507ca2c994ac0d143eeb21
lalet <laletscaria@yahoo.co.in>
2020-10-20 22:34:26 -0400

    ios changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.white,
+                child: Center(
+                  child: Text('Container 1'),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                child: Center(
+                  child: Text('Container 2'),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.green,
+                child: Center(
+                  child: Text('Container 3'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
